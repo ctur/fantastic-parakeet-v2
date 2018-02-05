557ec2e5a068ad835cc1c48873e968f01d582cb9
ctur <cturesoy@gmail.com>
2018-02-05 18:01:19 +0300

    packages added

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-1','env', 'es2017', 'stage-0']
+      }
+    }]
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
